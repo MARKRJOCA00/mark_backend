2c43547535937c857a5a83bf35b89bc2050a35ea
MARKRJOCA00 <MARKRJOCA00@gmail.com>
2024-05-19 23:23:01 +0800

    hahahaha grabe ito lang kaya ko nang hind gumgamit ng net whahahaha  yan lang mga na stock sa utak ko dami ko na nasa isip kaso diko kabisado yung sa mga  flexbox hahahahaa sad

--- app/server.js
+++ app/server.js
@@ -3,6 +3,8 @@ const path = require("path");
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.use(express.static(path.join(__dirname, 'public')));
+
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
